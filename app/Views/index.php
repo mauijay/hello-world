490a0da11613926a786d092dea64fb0f7a366227
Jay <jaycadla@gmail.com>
2025-06-26 09:40:41 -1000

    bump to ci 4.6.1

---
+++ app/Views/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>808BIZ-Jay</title>
+</head>
+<body>
+  <header>
+    <h1>808BIZ-Jay</h1>
+  </header>
+  <main>
+    <h2>Page Title</h2>
+    <p>Jay is a nice guy</p>
+  </main>
+  <footer>
+    <p>&copy;</p>
+  </footer>
+  <script src=""></script>
+</body>
+</html>
